6661b6e0d2987fd969879ef136662ea2cad3e18f
sofa <matassa.sophia@gmail.com>
2024-11-14 21:40:34 +0800

    01-02 completed

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,12 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return "ERROR"
+    }
+    let concat = "";
+    for (let i = 0; i < num; i++) {
+        concat = concat + string;
+    }
+    return concat;
 };
 
 // Do not edit below this line
