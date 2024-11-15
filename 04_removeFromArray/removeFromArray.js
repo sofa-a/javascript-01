a636fc566ae47f14bec4b50c5d74be1996a9c67b
sofa <matassa.sophia@gmail.com>
2024-11-15 17:36:23 +0800

    04 complete

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,15 @@
 const removeFromArray = function() {
+    let array = arguments[0];
+    if (arguments.length == 2) {
+        return array.filter(element => element !== arguments[1]);
+    }
+    else if (arguments.length > 2) {
+        let newArray = array;
+        for (let i=1; i<arguments.length; i++) {
+            newArray = newArray.filter(element => element !== arguments[i]);
+        }
+        return newArray;
+    }
 };
 
 // Do not edit below this line
