daea4d6ca1932b99e065c0ce98dbf989a79f5770
Sophia <20864846@student.curtin.edu.au>
2024-11-15 17:02:15 +0800

    03 part

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    const arr = string.split("");
+    let backwards = "";
+    for (let i=arr.length-1; i>-1; i--) {
+        backwards += arr[i]
+    }
+    return backwards;
 };
 
 // Do not edit below this line
