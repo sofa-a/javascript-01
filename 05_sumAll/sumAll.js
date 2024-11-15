14e17c2006d4f3e7460a2103cebb8527e1ee43c0
sofa <matassa.sophia@gmail.com>
2024-11-15 21:15:47 +0800

    05 completed

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,22 @@
 const sumAll = function() {
+    let small = arguments[0];
+    let large = arguments[1];
+    let sum = 0;
 
+    if (typeof small !== 'number' || typeof large !== 'number' ||
+        small < 0 || large < 0 || small % 1 !== 0 || large % 1 !== 0) {
+        return "ERROR";
+    }
+
+    if (arguments[0] > arguments[1]) {
+        small = arguments[1];
+        large = arguments[0];
+    }
+
+    for (let i=small; i<large+1; i++) {
+        sum += i;
+    } 
+    return sum;
 };
 
 // Do not edit below this line
