483faa912ec68caa00eb1b35505796064bdce5e2
sofa <matassa.sophia@gmail.com>
2024-11-15 21:42:40 +0800

    07 completed

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,15 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+    return round(((temp-32)*5)/9, 1);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+    return round(((temp*(9/5))+32), 1);
 };
+
+function round(number, places) {
+    const factor = Math.pow(10, places);
+    return Math.round(number * factor) / factor;
+}
 
 // Do not edit below this line
 module.exports = {
